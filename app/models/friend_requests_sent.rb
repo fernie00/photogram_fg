8af69b5275c36ba67c24ea7a71a0a6d79a557e61
Raghu Betina <raghu@firstdraft.com>
2017-09-30 06:38:45 +0000

    Validate uniqueness of friend_requests_sents.recipient_id

--- app/models/friend_requests_sent.rb
+++ app/models/friend_requests_sent.rb
@@ -5,6 +5,8 @@ class FriendRequestsSent < ApplicationRecord
 
   # Validations
 
+  validates :recipient_id, :uniqueness => { :scope => [:sender_id] }
+
   validates :recipient_id, :presence => true
 
 end
